cce7cfb74df0e7e75bfc3fe1717d5b5f54f36d04
Minkbear <minkbear@gmail.com>
2020-03-10 13:28:22 +0700

    move to folder

--- create_dockeruser.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-net user "DockerUser" "password987" /add
-net localgroup "Administrators" "DockerUser" /add
-WMIC USERACCOUNT WHERE "Name='DockerUser'" SET PasswordExpires=FALSE
-WMIC USERACCOUNT WHERE "Name='DockerUser'" SET Passwordchangeable=FALSE
