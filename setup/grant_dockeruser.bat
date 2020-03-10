db52481cac85242ceb4cb61f422d0a4e5b7aa5c0
Minkbear <minkbear@gmail.com>
2020-03-10 13:30:01 +0700

    correct path

--- setup/grant_dockeruser.bat
+++ setup/grant_dockeruser.bat
@@ -1,2 +1,2 @@
-icacls "D:\work\src" /q /c /t /grant DockerUser:F
-icacls "D:\work\docker_data" /q /c /t /grant DockerUser:F
+icacls "D:\src" /q /c /t /grant DockerUser:F
+icacls "D:\docker_data" /q /c /t /grant DockerUser:F
