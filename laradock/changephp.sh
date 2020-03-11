63563d6fc33e3ba9482e4c24a8e17e337306bf78
Minkbear <minkbear@gmail.com>
2020-03-11 14:32:30 +0700

    change php version for mac

---
+++ laradock/changephp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+version=$1
+echo 'changing Php Version to '$1;
+
+###change version in env file                        
+sed -i '.bak' "s/PHP_VERSION=.../PHP_VERSION=$1/g" '.env'                                               
+
+### Build fpm and cli for new version                       
+docker-compose build php-fpm                       
+docker-compose build workspace                                               
+
+### Restart Container to use new version of php
+docker-compose down                       
+sh ./start.sh
