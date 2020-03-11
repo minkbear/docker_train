e6c5c06ed79f2c5298b4b07fa0b7190415995d96
Minkbear <minkbear@gmail.com>
2020-03-11 16:04:10 +0700

    Update changephp.sh

--- laradock/changephp.sh
+++ laradock/changephp.sh
@@ -7,9 +7,24 @@ echo 'changing Php Version to '$1;
 sed -i '.bak' "s/PHP_VERSION=.../PHP_VERSION=$1/g" '.env'                                               
 
 ### Build fpm and cli for new version                       
-docker-compose build php-fpm                       
-docker-compose build workspace                                               
+docker-compose build --no-cache php-fpm                       
+docker-compose build --no-cache workspace                                               
 
 ### Restart Container to use new version of php
-docker-compose down                       
-sh ./start.sh
+docker-compose down
+
+if [ $1 = "5.6" ]
+then
+docker-compose build --no-cache mysql 
+
+docker-compose down
+docker-compose up -d nginx mysql
+fi
+
+if [ $1 = "7.3" ]
+then
+docker-compose build --no-cache mariadb
+
+docker-compose down
+docker-compose up -d nginx mariadb
+fi
